fac5ee029bba93eb3d135e3d7856768669a21408
Wain <tryn.1t.vii@gmail.com>
2024-10-02 16:08:12 +0600

    Mainbody fixed
    🥳

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -6,3 +6,24 @@ function Main() {
         document.body.className = 'light-theme';
     }
 }
+
+let srt = '9921';
+
+console.log( srt.at(0) ) ;
+console.log( srt.length );
+
+let first = 'COOKIES!!!!!';
+let second = 'COOK';
+
+if(first.at(0) === second.at(0)) {
+    console.log('Да они равны');
+} else {
+    console.log('Они не равны...');
+}
+let a = ('b' + 'a' + +'a' + 'a').toLowerCase();
+console.log(a);
+
+
+let b = 'asdadsad';
+
+console.log( b.substr(7,1)) ;
